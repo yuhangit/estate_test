22a0da21c88ef65ec4ac189c48d8317035eaf8f5
logan <gufengxiaoyuehan@163.com>
2018-03-20 19:58:10 +0800

    initialization

---
+++ src/main/scala/com/jiuzhong/Enc.scala
@@ -0,0 +1,93 @@
+package com.jiuzhong
+
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.util.Base64
+import scala.io.Source
+import java.security.MessageDigest
+import java.util
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+import java.io.PrintWriter
+//import org.apache.commons.codec.binary.Base64
+
+class Enc extends java.io.Serializable {
+  private val secret: String = "abcde12345!@#$%"
+  private val SALT: String = "jMhKlOuJnM34G6NHkqo9V010GhLAqOpF0BePojHgh1HgNg8^72k"
+  private val keyTOSpec = keyToSpec(secret)
+
+  def encrypt(value: String): String = {
+    val cipher: Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.ENCRYPT_MODE, keyTOSpec)
+    new String(Base64.getEncoder.encode(cipher.doFinal(value.getBytes("UTF-8"))))
+  }
+
+  def keyToSpec(key: String): SecretKeySpec = {
+    var keyBytes: Array[Byte] = (SALT + key).getBytes("UTF-8")
+    val sha: MessageDigest = MessageDigest.getInstance("SHA-1")
+    keyBytes = sha.digest(keyBytes)
+    keyBytes = util.Arrays.copyOf(keyBytes, 16)
+    new SecretKeySpec(keyBytes, "AES")
+  }
+
+  def decrypt(value: String):String = {
+    val key = secret
+    val cipher: Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.DECRYPT_MODE, keyTOSpec)
+    new String(cipher.doFinal(Base64.getDecoder.decode(value.getBytes())))
+  }
+
+
+}
+
+object Enc{
+  def encryptFile(enc: Enc,inputFile:String, outFile:PrintWriter): Unit ={
+    try{
+      for (line <- Source.fromFile(inputFile).getLines) {
+        val encStr = enc.encrypt(line)
+        outFile.write(encStr)
+        outFile.write("\n")
+      }
+    }catch {
+      case e: FileNotFoundException => println("Couldn't find that file.")
+      case e: IOException => println("Got an IOException!")
+    }finally {
+      outFile.close()
+    }
+  }
+
+  def decryptFile(enc: Enc,inputFile: String, outFile: PrintWriter): Unit ={
+    try{
+      for (line <- Source.fromFile(inputFile).getLines) {
+        val encStr = enc.decrypt(line)
+        outFile.write(encStr)
+        outFile.write("\n")
+      }
+    }catch {
+      case e: FileNotFoundException => println("Couldn't find that file.")
+      case e: IOException => println("Got an IOException!")
+    }finally {
+      outFile.close()
+    }
+  }
+  def main(args: Array[String]): Unit = {
+    val enc = new Enc()
+    val mode = args(0)
+    val inputFile = args(1)
+    val outFile = new PrintWriter(new File(args(2)))
+
+    if (mode == "0"){
+      encryptFile(enc,inputFile,outFile)
+    }else{
+      decryptFile(enc,inputFile,outFile)
+    }
+
+    //    val inputStr = args(0)
+    //    val outputStr = enc.encrypt(inputStr)
+    //    val encryStr = enc.encrypt(str)
+    //    println(encryStr)
+    //    println(enc.decrypt(encryStr))
+  }
+}
+
---
+++ src/main/scala/com/jiuzhong/Estate.scala
@@ -0,0 +1,47 @@
+package com.jiuzhong
+
+
+import java.text.SimpleDateFormat
+
+import org.apache.spark.sql.SparkSession
+
+object Estate {
+  val  spark = SparkSession.builder().appName("Estate").getOrCreate()
+  import spark.implicits._
+
+  val sc = spark.sparkContext
+  val today = new SimpleDateFormat("yyMMdd").format(new java.util.Date())
+
+  def scrapeSource(dt:String, enc: Enc): Unit ={
+    val publicPath = "hdfs://ns1/user/gdpi/public"
+    val addcookiePath = s"${publicPath}/sada_gdpi_adcookie/${dt}/*/*.gz"
+    val newclickPath = s"${publicPath}/sada_new_click/${dt}/*/*.gz"
+//    val postPath = s"${publicPath}/sada_gdpi_post_click/${dt}/*/*.gz"
+
+    val source = sc.textFile("%s,%s".format(addcookiePath,newclickPath))
+//    val source = sc.textFile("%s,%s".format(addcookiePath,newclickPath,postPath))
+
+    val urlPath = s"hdfs://ns1/user/u_tel_hlwb_mqj/private/estate/config/url.txt"
+    val savePath = s"hdfs://ns1/user/u_tel_hlwb_mqj/private/estate/${dt}/scrapeSource"
+
+    val urls: List[Array[String]] = sc.textFile(urlPath).map(l => enc.decrypt(l).split(" +")).collect().toList
+
+    val saveTbl = source.filter{
+      record =>
+        val arr = record.split("\t")
+        val sourceurl = arr(3)
+        val refurl = arr(4)
+        urls.exists(url => url.forall(part => sourceurl.contains(part) || refurl.contains(part)))
+    }.toDF()
+
+    saveTbl.coalesce(1000).write.format("com.databricks.spark.csv").
+      option("delimiter","\t").save(savePath)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val dateStr = args(0)
+    // don't initial class in class body
+    val enc = new Enc()
+    scrapeSource(dateStr,enc)
+  }
+}
