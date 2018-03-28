daf9976086d2a686559d52a83358ae0a5731e667
logan <gufengxiaoyuehan@163.com>
2018-03-28 15:04:18 +0800

    modified

--- src/main/scala/com/jiuzhong/Estate.scala
+++ src/main/scala/com/jiuzhong/Estate.scala
@@ -54,13 +54,14 @@ object Estate {
   }
 
 
-  def scrapeScource(adcookiePath:String, newclickPath:String, postPath:String, urlPath:String, savePath:String,enc:Enc): Unit = {
+  def scrapeScource(configMap:Map[String,String],enc:Enc): Unit = {
     //println(s"source files: ${adcookiePath} ${newclickPath} ${postPath}")
     val sadaRecordArr = Array("scrip", "ad", "ts", "url", "ref", "ua", "dstip","cookie", "srcPort")
     val decode64 = Base64.getDecoder
-    val data = sc.textFile("%s,%s".format(adcookiePath,newclickPath))
+
+    val data = sc.textFile(configMap("sourcePath"))
     //    val data = sc.textFile("%s,%s,%s".format(adcookiePath,newclickPath,postPath))
-    val urls = sc.textFile(urlPath).filter(_.trim != "").map(l => enc.decrypt(l).split(" +")).collect().toList
+    val urls = sc.textFile(configMap("urlPath")).filter(l => l.trim != "" && ! l.startsWith("#")).map(l => enc.decrypt(l).split(" +")).collect().toList
 
     val encryptString = udf{(url:String)=>
       enc.encrypt(url)
@@ -94,12 +95,12 @@ object Estate {
     //println("src data count"+ dataFilter.count)
     saveTbl.coalesce(1000).write.format("com.databricks.spark.csv").
       option("delimiter", varsMap("delm"))
-      .save(savePath)
+      .save(configMap("scrapePath"))
   }
 
   // filter data based ts col -- unxi epoch timestamp
   //case class Record(srcip:String, ad:String, ts:Long, url:String, ref:String, desip:String, cookie:String, src_port: String, tslag:Long);
-  def processSource(srcpath:String, destpath:String): Unit ={
+  def processSource(configMap:Map[String,String]): Unit ={
     // validinterval -- valid time interval in seconds
     // lowlimit -- min stay time in seconds
     // uplimit -- max stay time in seconds
@@ -127,30 +128,19 @@ object Estate {
 //    dataFilterMatch.write.format("com.databricks.spark.csv").option("delimiter",edelm).save(destMatchPath)
     //dataFilter.write.format("com.databricks.spark.csv").option("delimiter","\t").save(destpath)
     //println("after filter, data count: " + dataFilter.count)
-    val processDF = sc.textFile(srcpath).map{
+    val processDF = sc.textFile(configMap("scrapePath")).map{
         l =>
          val arr = l.split("\t")
         (arr(1),arr(5),arr(3))
     }.distinct().toDF("ad","ua","url")
-    processDF.coalesce(10).write.format("com.databricks.spark.csv").option("delimiter","\t").save(destpath)
+    processDF.coalesce(10).write.format("com.databricks.spark.csv").option("delimiter","\t").save(configMap("processPath"))
   }
-
-  def filterDataNew(srcpath:String, destPath:String, destMatchPath:String, paraFile:String): Unit ={
-    val delm = varsMap("delm")
-    val srcData = sc.textFile(srcpath).map{
-      row =>
-        val arr = row.split(delm)
-        (arr(0), arr(1),arr(2).toLong, arr(3).split("//")(1).split("/")(0), arr(4), arr(5), arr(6), arr(7), arr(8))
-    }.toDF("srcip","ad","ts","url","ref","ua","desip","cookie","src_port")
-    val params = sc.textFile(paraFile).map(l => l.split(" +")).collect()
-  }
-
 
   //case class Record(mobile:String, url:String)
-  def matchPortal(varsmap:Map[String,String], filterPath:String,  matchSaveFile:String,af:Int,pieceAmount:Int): Unit ={
+  def matchPortal( configMap:Map[String,String],af:Int,pieceAmount:Int): Unit ={
 
-    val delm = varsmap("delm")
-    val data = sc.textFile(filterPath).map{
+    val delm = varsMap("delm")
+    val data = sc.textFile(configMap("processPath")).map{
       row =>
         val arr =row.split(delm)
         (arr(0),arr(1),arr(2))
@@ -159,22 +149,21 @@ object Estate {
     import hlwbbigdata.phone
 
     val dataPieces =data.randomSplit(Array.fill(counts)(1))
-    for  ((piece,idx) <- dataPieces.zipWithIndex){
-      val matchResult = phone.phone_match(spark,piece, af.toString)
-      matchResult.write.format("com.databricks.spark.csv").option("delimiter", delm).save(matchSaveFile+ "_" + idx)
+    for  ((piece,idx) <- dataPieces.zipWithIndex) {
+      val matchResult = phone.phone_match(spark, piece, af.toString)
+      matchResult.write.format("com.databricks.spark.csv").option("delimiter", delm).save(configMap("matchPortalPath") + "_" + idx)
     }
 
+    val matchPortalPath = configMap("matchPortalPath")
+    val res = sc.textFile(matchPortalPath +"_*")
+    res.saveAsTextFile(matchPortalPath)
   }
 
-  def combinMatch(matchSaveFile:String): Unit ={
-    val res = sc.textFile(matchSaveFile+"_*")
-    res.saveAsTextFile(matchSaveFile)
-  }
 
-  def dropHistory(prjName:String,tagName:String,tagFile:String, mathePath:String,historyPath:String,outPath:String): Unit ={
+  def dropHistory(prjName:String,tagName:String, configMap:Map[String,String]): Unit ={
     val delm = varsMap("delm")
-    val tagNames =sc.textFile(tagFile).map(l => l.split(" +")).collect()
-    val inDF = sc.textFile(mathePath+"*").map(row => (row.split(delm)(0), row.split(delm)(1))).map{
+    val tagNames =sc.textFile(configMap("tagPath")).map(l => l.split(" +")).collect()
+    val inDF = sc.textFile(configMap("matchPortalPath")).map(row => (row.split(delm)(0), row.split(delm)(1))).map{
       row =>
         val appName = tagNames.filter(l => row._2.contains(l(0)) || l(0) == "*").
           map(l => l(1)).headOption.getOrElse("")
@@ -184,13 +173,13 @@ object Estate {
     val tagString = ":" + tagName + "_" + todayStr  + tagStringSuf(prjName)
 
 
-    val hisDF = sc.textFile(historyPath).map(row => row.split(delm)(0)).toDF("mobile")
+    val hisDF = sc.textFile(configMap("historyPath")).map(row => row.split(delm)(0)).toDF("mobile")
     val tagDF = inDF.join(hisDF,Seq("mobile"),"leftanti").withColumn("tag", concat($"url",lit(tagString))).drop($"url").dropDuplicates(Seq("mobile"))
-    tagDF.coalesce(1).write.format("com.databricks.spark.csv").option("delimiter",delm).save(outPath)
+    tagDF.coalesce(1).write.format("com.databricks.spark.csv").option("delimiter",delm).save(configMap("dropHistoryPath"))
 
   }
 
-  def kvTag( key:String, inPath:String, kvPath :String, outHistoryPath:String): Unit ={
+  def kvTag( key:String,configMap:Map[String,String]): Unit ={
     val delm = varsMap("delm")
     val underScore = varsMap("underscore")
     val ad  = varsMap("ad")
@@ -204,7 +193,7 @@ object Estate {
     //        (key + underScore + todayStr + underScore + row._2, ad + delm + row._1._2 + delm + row._1._1)
     //    ).toDF("key","value")
 
-    val inDF = sc.textFile(inPath).map{
+    val inDF = sc.textFile(configMap("dropHistoryPath")).map{
       row =>
         val arr = row.split(delm,2)
         (arr(0),arr(1))
@@ -217,12 +206,12 @@ object Estate {
 
     val kvTbl = inDF.union(counts)
     val historyDF = inDF.select("value").map(row => row.getString(0).split(delm)(2))
-    kvTbl.coalesce(1).write.format("com.databricks.spark.csv").option("delimiter",delm).save(kvPath)
-    historyDF.coalesce(1).write.format("com.databricks.spark.csv").option("delimiter",delm).save(outHistoryPath)
+    kvTbl.coalesce(1).write.format("com.databricks.spark.csv").option("delimiter",delm).save(configMap("kvPath"))
+    historyDF.coalesce(1).write.format("com.databricks.spark.csv").option("delimiter",delm).save(configMap("saveHistoryPath"))
 
   }
 
-  def getConfig(dateStr:String, prjName:String, method:String):Map[String,String]={
+  def getConfig(prjName:String, method:String,dateStr:String):Map[String,String]={
     // 得到每个项目不同方式下的配置文件: source file, url file, tag file
 
     val publicPath = "hdfs://ns1/user/gdpi/public"
@@ -232,6 +221,7 @@ object Estate {
     val postPath = s"${publicPath}/sada_gdpi_post_click/${dateStr}/*/*.gz"
     val sourcePath = "%s,%s,%s".format(addcookiePath, newclickPath, postPath)
 
+    val dateStrModified = dateStr.replace("*","_").replace("{","").replace("}","").replace(",","_")
     val user = System.getProperty("user.name")
     val privateBasePath = s"hdfs://ns1/user/${user}/private/test"
 
@@ -240,7 +230,7 @@ object Estate {
     val urlPath = s"${privateBasePath}/config/${prjName}_${method}_url.txt"
     val tagPath = s"${privateBasePath}/config/${prjName}_${method}_tag.txt"
     // private path
-    val privatePath = s"${privateBasePath}/${prjName}/${dateStr}/${method}"
+    val privatePath = s"${privateBasePath}/${prjName}/${dateStrModified}/${method}"
     val scrapePath = s"${privatePath}/scrape"
     val processPath = s"${privatePath}/process"
     val matchPortalPath = s"${privatePath}/match_portal"
@@ -248,13 +238,13 @@ object Estate {
     val kvPath = s"${privatePath}/kv"
     // history path
     val historyPath = s"${privateBasePath}/${prjName}_final_history/*"
-    val saveHistoryPath = s"${privateBasePath}/${prjName}_final_history/${method}_${dateStr}"
+    val saveHistoryPath = s"${privateBasePath}/${prjName}_final_history/${method}_${dateStrModified}"
 
     // default configuration return
     val baseCFG = Map(
       "sourcePath" -> sourcePath,
       "urlPath" -> urlPath,
-      "sctagPath" -> tagPath,
+      "tagPath" -> tagPath,
       "scrapePath" -> scrapePath,
       "processPath" -> processPath,
       "matchPortalPath" -> matchPortalPath,
@@ -274,7 +264,7 @@ object Estate {
           arr(0) -> arr(1)
       }.collect().toMap[String,String]
       baseCFG ++ cfg
-    }else if(sc.textFile(allCfgPath).filter(_.contains(s"${prjName}_${method}")).count() == 1) {
+    }else if( fs.exists(new Path(allCfgPath)) && sc.textFile(allCfgPath).filter(_.contains(s"${prjName}_${method}")).count() == 1) {
       val cfgPath = sc.textFile(allCfgPath).filter(!_.startsWith("#"))
         .filter(   _.contains(s"${prjName}_${method}")).take(1)(0).split(" +")(1)
 
@@ -290,6 +280,7 @@ object Estate {
         println("no config file find use default settings")
       baseCFG
     }
+    cfg.foreach(pair => println(pair._1+" : "+pair._2))
     cfg
 
 //    // history file path
@@ -298,6 +289,8 @@ object Estate {
   }
 
   def change_tag(filePath:String, newKey:String, newTag:String, newSubKey:String):Unit ={
+//    println("in change_tag ...")
+//    println(s"newkey: ${newKey}, newTag: ${newTag}, newSubKey: ${newSubKey}")
     val kv = sc.textFile(filePath).map {
       line =>
         val arr = line.split("\t",2)
@@ -320,22 +313,32 @@ object Estate {
           }
        arr.mkString("\t")
     }
-    deleteFile(filePath)
-    kv.saveAsTextFile(filePath)
+//    deleteFile(filePath)
+    kv.saveAsTextFile(filePath+s"_ChangeTag_${timeStr}")
+  }
+  def run_all_acc(prjName:String, method:String, dateStr:String,tagName:String,enc: Enc): Unit ={
+    val cfgs = getConfig(prjName,method,dateStr)
+    scrapeScource(cfgs,enc)
+    processSource(cfgs)
+    matchPortal(cfgs,0,10000)
+    dropHistory(prjName,tagName,cfgs)
+
   }
 
   // arg 0 : project name must supply.
   def main(args: Array[String]): Unit = {
     assert(args.length >= 1, "stage name must supply")
-    val stage = args(0)
-    val prjName = args(1)
-    val method = args(2)
-    val dateStr = if (args.length < 2) new SimpleDateFormat("yyyyMMdd").format(new java.util.Date()) else args(3)
+    val cmd = args(0)
+    val prjName = args.lift(1).getOrElse("")
+    val method = args.lift(2).getOrElse("")
+    val dateStr = if (args.length < 2) new SimpleDateFormat("yyyyMMdd").format(new java.util.Date()) else args.lift(3).getOrElse("")
 
     val enc = new Enc("a123bcd#$e45!@%","jnM34G6NHkqMhKlOuJo9VhLAqOpF0BePojHgh1010GHgNg8^72k")
 
-    stage match {
-      case "change_tag" => change_tag(args(1),args.lift(2).getOrElse(""),args.lift(3).getOrElse(""),args.lift(4).getOrElse(""))
+    (cmd,prjName,method,dateStr) match {
+      case ("test_conf",prj,mth,ds) => getConfig(ds,prj,mth)
+      case ("change_tag",arg1,arg2,arg3) => change_tag(arg1,arg2,arg3,args.lift(4).getOrElse(""))
+      case("run_all",arg1,"acc",arg3) => run_all_acc(arg1,"acc",arg3,args.lift(4).getOrElse("") ,enc)
     }
   }
 
